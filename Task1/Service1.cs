b7dce8d2cc0109d0ea17716e02654d8ef636c536
Konstantin <kostet2033@gmail.com>
2022-07-29 14:55:32 +0300

    Добавьте файлы проекта.

---
+++ Task1/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnStart(string[] args)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    File.AppendAllText(@"C:\Users\admin\Desktop\123.txt", "I'am working");
+                    Task.Delay(300);
+                }
+            });
+        }
+
+        protected async override void OnStop()
+        {
+            await Task.Run(() =>
+            {
+                File.AppendAllText(@"C:\Users\admin\Desktop\123.txt", "I'am stop" );
+            });
+        }
+    }
+}
